69a155f0c97634e816d60e39fd1d385086a8b053
Nishant Dixit <nd221619@gmail.com>
2023-07-24 23:53:32 +0530

    about us changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,16 +3,18 @@ import { AiFillInstagram, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { BiCoffeeTogo } from "react-icons/bi";
 
 const About = () => {
+
   return (
     <div>
       <div className=" p-4 flex-col text-base justify-between bg-[#222222] rounded-lg mx-3">
         <div className="mx-3">
           <a
-            href="#"
-            className="card w-auto  h-auto py-2 text-lg px-3 hover:bg-[#0c0c0c] bg-[#2c2c2c] border-white border-2 rounded-md m-1 flex items-center gap-1"
+            
+            className="card w-auto  hover:cursor-pointer h-auto py-2 text-lg px-3 hover:bg-[#0c0c0c] bg-[#2c2c2c] border-white border-2 rounded-md m-1 flex items-center gap-1"
           >
             Buy me a coffee <BiCoffeeTogo />
           </a>
+          <p className=" text-sm my-2 text-red-400">Payment Gateway will be added soon, till then enjoy using the extension :D</p>
           <p className="mt-5">Access all your extension in a list without switching tabs.</p>
           <p className="font-bold mb-2">
             Pin the extension for more productivity!
@@ -21,9 +23,9 @@ const About = () => {
           <hr />
           <ul className="list-disc my-2">
             <li>Bookmarks are fetched from Browser itself</li>
-            <li>Bookmark a page to access it from the extension</li>
+            <li>Bookmark a page in the browser to access it from the extension</li>
             <li>If you can't access a url or icon maybe the url is not working</li>
-            <li>Ctrl+Shift+Q to open the extension</li>
+            <li>Ctrl+Shift+Q to open the extension </li>
           </ul>
         </div>
         <div className="py-2 mt-5">
