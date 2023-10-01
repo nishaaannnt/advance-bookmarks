91404911cab9014f22511b38a5e8554bd539ceb0
Raghav Goel <goelraghav002@gmail.com>
2023-10-01 20:53:55 +0530

    fixed code errors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,12 +8,12 @@ const About = () => {
     <div>
       <div className=" p-4 flex-col text-base justify-between bg-[#222222] rounded-lg mx-3">
         <div className="mx-3">
-          <a
+          <button
             
             className="card w-auto  hover:cursor-pointer h-auto py-2 text-lg px-3 hover:bg-[#0c0c0c] bg-[#2c2c2c] border-white border-2 rounded-md m-1 flex items-center gap-1"
           >
             Buy me a coffee <BiCoffeeTogo />
-          </a>
+          </button>
           <p className=" text-sm my-2 text-red-400">Payment Gateway will be added soon, till then enjoy using the extension :D</p>
           <p className="mt-5">Access all your extension in a list without switching tabs.</p>
           <p className="font-bold mb-2">
@@ -31,19 +31,19 @@ const About = () => {
         <div className="py-2 mt-5">
           <ul className="flex items-center w-full justify-between bottom-0">
             <li className="px-3">
-              <a href="https://twitter.com/xdiziz" target="_blank" className="flex items-center gap-1">
+              <a href="https://twitter.com/xdiziz" target="_blank" rel="noreferrer" className="flex items-center gap-1">
                 <AiFillTwitterCircle />
                 Twitter
               </a>
             </li>
             <li className="px-3">
-              <a href="https://github.com/nishaaannnt/advance-bookmarks" target="_blank" className="flex items-center gap-1">
+              <a href="https://github.com/nishaaannnt/advance-bookmarks" target="_blank" rel="noreferrer" className="flex items-center gap-1">
                 <AiFillGithub />
                 Github
               </a>
             </li>
             <li className="px-3">
-              <a href="https://www.linkedin.com/in/nishantdixitt/" target="_blank" className="flex items-center gap-1">
+              <a href="https://www.linkedin.com/in/nishantdixitt/" rel="noreferrer" target="_blank" className="flex items-center gap-1">
                 <AiFillLinkedin />
                 LinkedIn
               </a>
